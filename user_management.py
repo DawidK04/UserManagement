5ceb843d6e19dbbb805ae8cd94335e33e5d7ad45
DawidK04 <karolakdawid04@gmail.com>
2024-12-03 09:28:30 +0100

    Add files via upload

---
+++ user_management.py
@@ -0,0 +1,26 @@
+import json
+import random
+import re
+import os
+
+
+
+def validate_pesel(pesel):
+    pass
+
+def validate_nip(nip):
+    pass
+
+def validate_regon(regon):
+    pass
+
+def add_user(user_data):
+    """
+    Adds a new user to 'users.json' file after data validation'
+    """
+    pass
+
+
+
+def save_users_to_file(users):
+    pass
